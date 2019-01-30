8eb63ecc9d8a987bd981d50ffbe668b9a43d02d5
Chandra Prakash <chanprakash@Chandras-MacBook-Pro.local>
2019-01-30 17:03:16 +0530

    Clean up idea project for java

---
+++ LANGUAGE PROFICIENCY/Java/Introduction/WelcomeToJava.java
@@ -0,0 +1,6 @@
+public class WelcomeToJava {
+  public static void main(String [] args) {
+    System.out.println("Hello, World.");
+    System.out.println("Hello, Java.");
+  }
+}
--- LANGUAGE PROFICIENCY/Java/src/main/java/com/hackerrank/languages/introduction/WelcomeToJava.java
+++
@@ -1,8 +0,0 @@
-package com.hackerrank.languages.introduction;
-
-public class WelcomeToJava {
-    public static void main(String [] args) {
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
-    }
-}

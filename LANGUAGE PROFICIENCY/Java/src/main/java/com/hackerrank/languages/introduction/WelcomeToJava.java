93c0b766ac5089bbb859a3ebdacf81248d9b6d6c
Chandra Prakash <chanprakash@Chandras-MacBook-Pro.local>
2019-01-30 16:18:46 +0530

    Add First Java solution

---
+++ LANGUAGE PROFICIENCY/Java/src/main/java/com/hackerrank/languages/introduction/WelcomeToJava.java
@@ -0,0 +1,8 @@
+package com.hackerrank.languages.introduction;
+
+public class WelcomeToJava {
+    public static void main(String [] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}

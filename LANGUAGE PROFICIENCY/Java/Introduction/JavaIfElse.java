0614377642d8f7632138f9f50add9f64852bb70a
Chandra Prakash Reddy <pr.chandraprakash@gmail.com>
2019-02-02 11:36:34 +0530

    improved logic in solution

--- LANGUAGE PROFICIENCY/Java/Introduction/JavaIfElse.java
+++ LANGUAGE PROFICIENCY/Java/Introduction/JavaIfElse.java
@@ -18,9 +18,7 @@ public class Solution {
         if(N % 2 == 1) {
             System.out.println("Weird");
         } else {
-            if (N >=2 && N <=5) {
-                System.out.println("Not Weird");
-            } else if (N >= 6 && N <= 20) {
+            if (N >= 6 && N <= 20) {
                 System.out.println("Weird");
             } else {
                 System.out.println("Not Weird");

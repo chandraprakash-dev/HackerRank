b5acdf6639ef9ff397490d025cc6a2b2b86cc0ae
CodeSurfer3022 <pr.chandraprakash@gmail.com>
2019-02-27 21:26:55 +0530

    Updated 100% working solution for Querying the document in c functions

--- LANGUAGE PROFICIENCY/C/Functions/QueryingTheDocument.c
+++ LANGUAGE PROFICIENCY/C/Functions/QueryingTheDocument.c
@@ -6,74 +6,73 @@
 #define MAX_PARAGRAPHS 5
 
 char* kth_word_in_mth_sentence_of_nth_paragraph(char**** document, int k, int m, int n) {
-    return document[n][m][k];
+    return document[n - 1][m - 1][k - 1];
 }
 
 char** kth_sentence_in_mth_paragraph(char**** document, int k, int m) {
-    return document[m][k];
+    return document[m - 1][k - 1];
 }
 
 char*** kth_paragraph(char**** document, int k) {
-    return document[k];
+    return document[k - 1];
 }
 
 char**** get_document(char* text) {
-    char ****doc = malloc(MAX_PARAGRAPHS * sizeof(char ***));
+    // doc points to an array of paragraphs
+    char ****doc = malloc(MAX_PARAGRAPHS * sizeof(char ***)); 
+
+    // for each paragraph, assign an array of 1 sentence, we can reallocate later
     for(int i = 0; i < MAX_PARAGRAPHS; i ++) {
         doc[i] = malloc(1 * sizeof(char **));
     }
+
+    // for each sentence assign an array of 1 word
     for(int i = 0; i < MAX_PARAGRAPHS; i ++) {
-            for(int j = 0; j < 1; j ++) {
-                    doc[i][j] = malloc(1 * sizeof(char*));
-            }
-    }
-    for(int i = 0; i < MAX_PARAGRAPHS; i ++) {
-            for(int j = 0; j < 1; j ++) {
-                    for(int k = 0; k < 1; k ++) {
-                            doc[i][j][k] = malloc(1 * sizeof(char));
-                    }
-            }
+        for(int j = 0; j < 1; j ++) {
+            doc[i][j] = malloc(1 * sizeof(char*));
+        }
     }
 
-
-    for(int n = 0, i = 0, j = 0, k = 0, l = 0; i < strlen(text); n ++) {
+    // for each word assign an array of 1 character
+    for(int i = 0; i < MAX_PARAGRAPHS; i ++) {
+        for(int j = 0; j < 1; j ++) {
+            for(int k = 0; k < 1; k ++) {
+                doc[i][j][k] = malloc(1 * sizeof(char));
+            }
+        }
+    }
+     
+    for(int n = 0, i = 0, j = 0, k = 0, l = 0; n < strlen(text); n ++) {
         if(text[n] != ' ' && text[n] != '\n' && text[n] != '.') {
             doc[i][j][k][l] = text[n];
             l++;
-            doc[i][j][k] = realloc(doc[i][j][k], l + 1);
-
-        }
-
-        if(text[n] == ' ') {
+            doc[i][j][k] = realloc(doc[i][j][k], (l + 1) * sizeof(char));
+        } else if(text[n] == ' ') {
             doc[i][j][k][l] = '\0';
             l = 0;
             k++;
-            doc[i][j] = realloc(doc[i][j], k + 1);
+            doc[i][j] = realloc(doc[i][j], (k + 1) * sizeof(char*));
+            doc[i][j][k] = malloc(sizeof(char));
             continue;
-        }
-
-        if(text[n] == '.') {
-            doc[i][j][k][l] = text[n];
-
+        } else if(text[n] == '.') {
+            doc[i][j][k][l] = '\0';
             k = l = 0;
             j++;
+            doc[i] = realloc(doc[i], (j+1) * sizeof(char**));
+            doc[i][j] = malloc(sizeof(char*));
+            doc[i][j][k] = malloc(sizeof(char));
             continue;
-        }
-
-        if(text[n] == '\n') {
-            doc[i][j][k][l] = text[n];
-            doc[i] = realloc(doc[i], j + 1);
+        } else if(text[n] == '\n') {
             j = k = l = 0;
             i++;
             continue;
         }
     }
-    printf("%c", doc[0][0][0][0]);
     return doc;
 }
 
 
-char* get_input_text() {
+char* get_input_text() {	
     int paragraph_count;
     scanf("%d", &paragraph_count);
 
@@ -104,7 +103,7 @@ void print_sentence(char** sentence) {
         if( i != word_count - 1)
             printf(" ");
     }
-}
+} 
 
 void print_paragraph(char*** paragraph) {
     int sentence_count;
@@ -115,7 +114,7 @@ void print_paragraph(char*** paragraph) {
     }
 }
 
-int main()
+int main() 
 {
     char* text = get_input_text();
     char**** document = get_document(text);
@@ -148,6 +147,5 @@ int main()
             print_paragraph(paragraph);
         }
         printf("\n");
-    }
+    }     
 }
-
